69fc4dc7239b2c63b7fb4f0a5eb870826b1a35aa
SilasCh <silas.b.christiansen@gmail.com>
2021-11-18 10:54:16 +0100

    Added function that picks a random pupil from a pupil list

--- Main/RandGen.ino
+++ Main/RandGen.ino
@@ -4,3 +4,11 @@ int RandGenerator(int lower, int upper)
   int randNumber = random(lower, upper);
   return randNumber;
 }
+
+char classState()
+{
+  const char *classList[] = {"Anders", "Emil", "Fahmi", "Freja", "Gustav W", "Gustav E", "Haris", "Ismail", "Jabriil", "Jacob", "Ludvig", "Jeppe", "Jonatan", "Kasper", "Lovro", "Mathias", "Mie", "Mohammad", "Nelisa", "Nikhil", "Rasmus", "Robert", "Pernille", "Sarah", "Silas", "Simon", "Thøger", "Tobias", "Taaha", "William"};
+  int pupilNumber = RandGenerator(0,29);
+  char pupil = *classList[pupilNumber];
+  return pupil;
+}
